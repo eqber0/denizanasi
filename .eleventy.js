2d40d7335afb3e404deaafdae546501a5fce5ecb
eqber00 <eqber00@gmail.com>
2023-07-11 22:26:12 +0300

    fixed cms

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images")
   eleventyConfig.addPassthroughCopy("./src/wp-content")
   eleventyConfig.addPassthroughCopy("./src/wp-includes")
+  eleventyConfig.addPassthroughCopy("./src/admin")
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
